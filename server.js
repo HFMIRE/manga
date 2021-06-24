c7a95198551f69d14cc867a274d4f3068708ae04
HFMIRE <huda.f.mire@gmail.com>
2021-06-24 19:08:51 +0100

    Card format

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
-// manga - create  update and edit and delete - manga recomedation - list npm
-const title = document.getElementById(".title");
-const poster = document.getElementById(".poster");
-// picture + name
-function getMangaData() {
-  // anime?page[limit]=20&page[offset]=0
-  axios
-    .get(`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0`)
-    .then(({ data }) => {
-      console.log(data.data);
-      const html = data.data
-        .map((item) => {
-          return `
-            <div class="item">
-            <img src=${item.attributes.posterImage.small}/> 
-            <h2>${item.attributes.titles.en_jp}</h2>
-            <h5>${item.attributes.titles.ja_jp}</h5> <span> <p>${item.attributes.startDate}</p>
-            <h2>Total Episodes: ${item.attributes.episodeLength}</h2>
-            <p>${item.attributes.description}</p> 
-            </div>
-            `;
-        })
-        .join(" ");
-      document.querySelector("#app").insertAdjacentHTML("afterend", html);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+ const app = document.querySelector(".app")
 
-getMangaData();
+
+ function getMangaData() {
+     axios
+         .get(`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0`)
+         .then(({ data }) => {
+             console.log(data.data);
+             const html = data.data
+             let htmlString = ""
+             const itemInfo = html.forEach((item, index) => {
+                 htmlString += `
+                 <div class="card" style="width:400px">
+                 <img src="${item.attributes.posterImage.small}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                   <h5 class="card-title">${item.attributes.titles.en_jp}</h5>
+                   <div class="article">
+                   <div class="text short">
+                   <p class="card-text">${item.attributes.description}</p>
+                 </div>
+                 <span class="read-more">read more</span>
+                 </div>
+                 </div>
+                 </div>
+              `;
+             })
+             app.innerHTML = htmlString;
+         })
+         .catch((error) => {
+             console.log(error);
+         });
+ }
+
+ getMangaData();
+
+ $(document).ready(function() {
+     $(".read-more").click(function() {
+         var $elem = $(this).parent().find(".text");
+         if ($elem.hasClass("short")) {
+             $elem.removeClass("short").addClass("full");
+         } else {
+             $elem.removeClass("full").addClass("short");
+         }
+     });
+ });
